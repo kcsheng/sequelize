c69f4d5731d58ec3e7b3bc931db2157fc3807145
kcsheng <kacysheng@mail.com>
2021-08-11 15:53:06 +1000

    Add hooks

--- app.js
+++ app.js
@@ -14,27 +14,26 @@ const Article = connection.define(
     },
     title: {
       type: DataTypes.STRING,
-      unique: true,
-      allowNull: false,
-      validate: {
-        len: {
-          args: [10, 100],
-          msg: "Please enter a message that is longer than 10 but shorter than 150 characters.",
-        },
-      },
     },
     body: {
       type: DataTypes.TEXT,
-      // defaultValue: "Not yet completed...",
-      validate: {
-        startsWithCapital(value) {
-          const first = value.charAt(0);
-          if (first !== first.toUpperCase()) {
-            throw new Error(
-              "Your article content must start with capital letter."
-            );
-          }
-        },
+    },
+  },
+  {
+    hooks: {
+      beforeValidate() {
+        console.log("This shows for beforeValidate");
+      },
+      afterValidate() {
+        console.log("THis shows for aftervalidate");
+      },
+      beforeCreate() {
+        console.log("This shows for beforeCreate");
+      },
+      afterCreate(obj) {
+        console.log(
+          `This shows for aftercreate. The result has slug as ${obj.dataValues.slug}`
+        );
       },
     },
   },
@@ -44,37 +43,13 @@ const Article = connection.define(
   }
 );
 
-// connection
-//   .sync()
-//   .then(() => {
-//     Article.create({
-//       title: "demo title 1",
-//       body: "lorem ipsum lots of text lorem ipsum lots of textots of text lorem ipsum lots off text lorem ipsum lots of text lorem ipsum lots of text lorem ipsum lots of text lorem ipsum lots of text lorem ipsum lots of text lorem ipsum lots of text m lott lorem ipsum lots of textots of text lorem ipsum lots off text lorem ipsum lots of text lorem ipsum lots ofs of text lorem ipsum lots of text lorem ipsum lots o",
-//     });
-//   })
-//   .catch();
-
-// connection.sync() alone syncing the model to the database and generate the table in the database
-// I then created the demo_schema database using the sql command prompt
-// After that I execute "node app" and the table is created.
-// Note that sequalize took the liberty to generate id, created_at and updated_at as well.
-// I then added .then() to insert data (why? because creatign a table takes time, so Article.create is basically a callback function that consumes the promise.)
-// Run the node app again, you can see the insert query executed in the command line. One article has been inserted.
-
-// connection
-//   .sync()
-//   .then(() => {
-//     Article.findByPk(3).then((article) => console.log(article.dataValues));
-//   })
-//   .catch();
-
 connection
-  .sync({ force: true, logging: console.log })
+  .sync({ force: true })
   .then(() => {
     Article.create({
-      slug: "wibble",
-      title: "wiggle ooooooooooo",
-      body: "Wobble ooo",
+      slug: "This is Slug",
+      title: "This is title",
+      body: "This is the body...",
     });
   })
   .catch((err) => console.error(err));
