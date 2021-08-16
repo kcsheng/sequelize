c26c1b7f53a785a346815314bd17c25404268437
kcsheng <kacysheng@mail.com>
2021-08-17 09:13:20 +1000

    aync await to check connection

--- app.js
+++ app.js
@@ -1,56 +1,22 @@
-const { Sequelize, Op, Model, DataTypes } = require("sequelize");
+const { Sequelize } = require("sequelize");
 
-const connection = new Sequelize("demo_schema", "root", "", {
+const sequelize = new Sequelize("demo_schema", "root", "", {
   host: "localhost",
   dialect: "mysql",
+  password: "12345678",
 });
+// sequelize
+//   .authenticate()
+//   .then(() => {
+//     console.log("Sequelze connected ...");
+//   })
+//   .catch((err) => console.error(err));
 
-const Article = connection.define("article", {
-  title: {
-    type: DataTypes.STRING,
-    primaryKey: true,
-  },
-  content: {
-    type: DataTypes.TEXT,
-  },
-  approved: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: false,
-  },
-});
+// This will work too
 
-connection
-  .sync({ force: true })
-  .then(() => {
-    const req = {
-      body: {
-        title: "some title",
-        content: "some content",
-        approved: true,
-      },
-    };
-    // Bulkcreate using json syntax
-    Article.bulkCreate(
-      [
-        {
-          title: "This is title1",
-          content: "This is content1",
-        },
-        {
-          title: "This is title2",
-          content: "This is content2",
-        },
-        {
-          title: "This is title3",
-          content: "This is content3",
-        },
-      ],
-      {
-        validate: true,
-        ignoreDuplicate: true,
-      }
-    ).then((article) => {
-      console.log(article.dataValues);
-    });
-  })
-  .catch((err) => console.error(err));
+const sequelConnect = async () => {
+  await sequelize.authenticate();
+  console.log("Connection successful...");
+};
+
+sequelConnect();
